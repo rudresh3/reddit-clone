cda6ca605f641b009dd508f2e8a73669f4e63c65
rudresh3 <rudreshrush@gmail.com>
2023-05-02 23:49:39 +0530

    first

---
+++ src/chakra/theme.ts
@@ -0,0 +1,23 @@
+import '@fontsource/open-sans/300.css'
+import '@fontsource/open-sans/400.css'
+import '@fontsource/open-sans/700.css'
+import { extendTheme } from '@chakra-ui/react'
+
+export const theme = extendTheme({
+  colors: {
+    brand: {
+      100: '#ff3c00',
+    },
+  },
+  font: {
+    body: 'Open Sans, sans-serif',
+  },
+  style: {
+    global: () => ({
+      body: {
+        bg: 'gray.200',
+      },
+    }),
+  },
+  components: {},
+})
